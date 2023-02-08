2ff52c98bf617646cc261725b1d09b8d0fcb754b
iseolin <iseolin76@quantumuniverse.co>
2023-02-08 16:46:37 +0900

    init

---
+++ src/router.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <div>Hello world</div>,
+  },
+]);
+
+export default router;
